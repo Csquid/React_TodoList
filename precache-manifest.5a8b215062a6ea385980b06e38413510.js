f7525a4333af6b78eb4288f2a399c3a7b5e51e64
CodeMonkey <dmsqlctnekf@gmail.com>
2019-12-04 23:39:12 +0900

    Updates

---
+++ precache-manifest.5a8b215062a6ea385980b06e38413510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5c64481f142f829979bdbd9fd12154",
+    "url": "/React_TodoList/index.html"
+  },
+  {
+    "revision": "630aa3f5ffcdba0e5714",
+    "url": "/React_TodoList/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0f2a7b63873af0279f85",
+    "url": "/React_TodoList/static/css/main.aab57820.chunk.css"
+  },
+  {
+    "revision": "630aa3f5ffcdba0e5714",
+    "url": "/React_TodoList/static/js/2.0d61fec8.chunk.js"
+  },
+  {
+    "revision": "0f2a7b63873af0279f85",
+    "url": "/React_TodoList/static/js/main.dca2377f.chunk.js"
+  },
+  {
+    "revision": "52cb29e256ea5f0860d0",
+    "url": "/React_TodoList/static/js/runtime-main.93c51356.js"
+  }
+]);
